5a4381e042a43e50491e95219551f6493e3bd755
Tran Danh Doanh <nhimcaptin@gmail.com>
2026-08-22 16:00:59 +0700

    [Fix] fix power when create player

--- src/database/PlayerDAO.java
+++ src/database/PlayerDAO.java
@@ -312,10 +312,10 @@ private static final ScheduledExecutorService scheduler = Executors.newScheduled
                     + "data_inventory, data_location, data_point, data_magic_tree, items_body, "
                     + "items_bag, items_box, items_box_lucky_round, items_daban, friends, enemies, data_intrinsic, data_item_time,"
                     + "data_task, data_mabu_egg, data_charm, skills, skills_shortcut, pet,"
-                    + "data_black_ball, data_side_task, BoughtSkill, dailyGift) "
-                    + "values ()", userId, name, hair, gender, 0, -1, inventory, location, point, magicTree,
+                    + "data_black_ball, data_side_task, BoughtSkill, dailyGift, power) "
+                    + "values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", userId, name, hair, gender, 0, -1, inventory, location, point, magicTree,
                     itemsBody, itemsBag, itemsBox, itemsBoxLuckyRound, itemsDaBan, friends, enemies, intrinsic,
-                    itemTime, task, mabuEgg, charms, skills, skillsShortcut, petData, dataBlackBall, dataSideTask, dataBoughtSkill, dailyGift);
+                    itemTime, task, mabuEgg, charms, skills, skillsShortcut, petData, dataBlackBall, dataSideTask, dataBoughtSkill, dailyGift, 0);
             Logger.success("Tạo player mới thành công!\n");
             return true;
         } catch (Exception e) {
