e172a3e56e74099289a94f5485be97692c226d09
Tran Danh Doanh <nhimcaptin@gmail.com>
2026-08-22 14:49:18 +0700

    [Update] npc santa

--- src/npc/list/Santa.java
+++ src/npc/list/Santa.java
@@ -1,15 +1,12 @@
 package npc.list;
 
 import consts.ConstNpc;
-import item.Item;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import npc.Npc;
 import player.Player;
 import shop.ShopService;
-import services.func.Input;
-import player.Service.InventoryService;
 
 public class Santa extends Npc {
 
@@ -20,25 +17,19 @@ public class Santa extends Npc {
     @Override
     public void openBaseMenu(Player player) {
         if (canOpenNpc(player)) {
-
-            Item pGG = InventoryService.gI().findItem(player.inventory.itemsBag, 459);
-            int soLuong = 0;
-            if (pGG != null) {
-                soLuong = pGG.quantity;
-            }
             List<String> menu = new ArrayList<>(Arrays.asList(
                     "Cửa hàng",
                     "Mở rộng\nHành trang\nRương đồ",
-                    "Nhập mã\nquà tặng",
+                    // "Nhập mã\nquà tặng",
                     "Cửa hàng\nHạn sử dụng",
                     "Tiệm\nHớt tóc",
-                    "Danh\nhiệu",
+                    // "Danh\nhiệu",
                     "Shop Vip"
             ));
 
-            if (soLuong >= 1) {
-                menu.add(1, "Giảm giá\n80%");
-            }
+            // if (soLuong >= 1) {
+            //     menu.add(1, "Giảm giá\n80%");
+            // }
 
             String[] menus = menu.toArray(new String[0]);
 
@@ -51,12 +42,6 @@ public class Santa extends Npc {
     @Override
     public void confirmMenu(Player player, int select) {
         if (canOpenNpc(player)) {
-            Item pGG = InventoryService.gI().findItem(player.inventory.itemsBag, 459);
-            int soLuong = 0;
-            if (pGG != null) {
-                soLuong = pGG.quantity;
-            }
-
             if (this.mapId == 5 || this.mapId == 13 || this.mapId == 20) {
                 if (player.idMark.isBaseMenu()) {
                     switch (select) {
@@ -64,48 +49,15 @@ public class Santa extends Npc {
                             ShopService.gI().opendShop(player, "SANTA", false);
                             break;
                         case 1:
-                            if (soLuong >= 1) {
-                                ShopService.gI().opendShop(player, "SANTA_GIAM_GIA", false);
-                            } else {
-                                ShopService.gI().opendShop(player, "SANTA_MO_RONG_HANH_TRANG", false);
-                            }
+                            ShopService.gI().opendShop(player, "SANTA_MO_RONG_HANH_TRANG", false);
                             break;
                         case 2:
-                            if (soLuong >= 1) {
-                                ShopService.gI().opendShop(player, "SANTA_MO_RONG_HANH_TRANG", false);
-                            } else {
-                                  Input.gI().createFormGiftCode(player);
-                            }
+                            ShopService.gI().opendShop(player, "SANTA_HAN_SU_DUNG", false);
                             break;
                         case 3:
-                            if (soLuong >= 1) {
-                                  Input.gI().createFormGiftCode(player);
-                            } else {
-                                ShopService.gI().opendShop(player, "SANTA_HAN_SU_DUNG", false);
-                            }
+                            ShopService.gI().opendShop(player, "SANTA_HEAD", false);
                             break;
                         case 4:
-                            if (soLuong >= 1) {
-                                ShopService.gI().opendShop(player, "SANTA_HAN_SU_DUNG", false);
-                            } else {
-                                ShopService.gI().opendShop(player, "SANTA_HEAD", false);
-                            }
-                            break;
-                        case 5:
-                            if (soLuong >= 1) {
-                                ShopService.gI().opendShop(player, "SANTA_HEAD", false);
-                            } else {
-                                ShopService.gI().opendShop(player, "SANTA_DANH_HIEU", false);
-                            }
-                            break;
-                        case 6:
-                            if (soLuong >= 1) {
-                                ShopService.gI().opendShop(player, "SANTA_DANH_HIEU", false);
-                            } else {
-                                ShopService.gI().opendShop(player, "SHOP_VIP", false);
-                            }                            
-                            break;
-                        case 7:
                             ShopService.gI().opendShop(player, "SHOP_VIP", false);
                             break;
                     }
